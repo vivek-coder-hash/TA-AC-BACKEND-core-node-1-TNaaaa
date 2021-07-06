d6f4178022b34aa994c44943f4618a54cf24c1b8
vivek sharma <vivekslim@gmail.com>
2021-07-06 12:41:04 +0530

    assignment1

---
+++ block-BNaaaj/area.js
@@ -0,0 +1,21 @@
+function square(side) {
+    return side*side
+}
+
+
+function rectangle(a,b) {
+    return a*b
+}
+
+
+function circle(radius) {
+    return 3.14*radius*radius
+}
+
+
+module.exports= {
+    square: square ,
+    rectangle:rectangle ,
+    circle:circle
+
+}
--- block-BNaaaj/index.js
+++ block-BNaaaj/index.js
@@ -1,6 +1,11 @@
 console.log('Welcome to Nodejs')
 
 var os = require("os")
-console.log(os.freemem)
-console.log(os.uptime)
+console.log(os.cpus().length)
+console.log(os.freemem())
+console.log(os.uptime())
 console.log(os.version)
+
+
+var {readFile , unlink} = require("fs")
+
